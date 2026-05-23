2a1df2a09a291e33a9a2ee22720ff2e07800f0ce
Sreeram Cheekatla <ramsreeram249@gmail.com>
2026-05-23 14:16:40 +0530

    implemented spacing core functionality

---
+++ src/design/spacing.js
@@ -0,0 +1,20 @@
+export const spacing = {
+  none: "0px",
+  xs: "4px",
+  sm: "8px",
+  md: "12px",
+  lg: "16px",
+  xl: "24px",
+  xxl: "32px",
+  "3xl": "48px",
+  "4xl": "64px",
+  layout: {
+    paddingMobile: "16px",
+    paddingDesktop: "32px",
+    sidebarWidthClosed: "80px",
+    sidebarWidthOpen: "260px",
+    topbarHeight: "72px",
+  }
+};
+
+export default spacing;
