8444d578bf4f6ad6519bb022be88a74eb0453adb
Sreeram Cheekatla <ramsreeram249@gmail.com>
2026-05-23 18:32:18 +0530

    implemented typography core functionality

---
+++ src/design/typography.js
@@ -0,0 +1,36 @@
+export const typography = {
+  fontFamily: {
+    sans: '"Inter", "SF Pro Display", "-apple-system", BlinkMacSystemFont, sans-serif',
+    heading: '"Space Grotesk", "Inter", sans-serif',
+    mono: '"JetBrains Mono", monospace',
+  },
+  fontSize: {
+    xs: "0.75rem", // 12px
+    sm: "0.875rem", // 14px
+    base: "1rem", // 16px
+    lg: "1.125rem", // 18px
+    xl: "1.25rem", // 20px
+    xxl: "1.5rem", // 24px
+    "3xl": "1.875rem", // 30px
+    "4xl": "2.25rem", // 36px
+    "5xl": "3rem", // 48px
+  },
+  fontWeight: {
+    normal: "400",
+    medium: "500",
+    semibold: "600",
+    bold: "700",
+    extrabold: "800",
+    black: "900",
+  },
+  lineHeight: {
+    none: "1",
+    tight: "1.25",
+    snug: "1.375",
+    normal: "1.5",
+    relaxed: "1.625",
+    loose: "2",
+  }
+};
+
+export default typography;
